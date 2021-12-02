9898f6ea30a58c98b02237578aa83e8206bde1f0
web77 <web77@yandex.ru>
2021-12-02 10:44:23 +0300

    Change logic for showError function

--- src/components/api.js
+++ src/components/api.js
@@ -9,7 +9,7 @@ export class Api {
   _checkResponse(res) {
     if (res.ok) return res.json();
 
-    return Promise.reject(`Ошибка: ${res.status}`);
+    return Promise.reject(res);
   }
 
   getUserInfo() {
--- src/components/config.js
+++ src/components/config.js
@@ -26,6 +26,7 @@ export const config = {
   form: {
     inputSelector: ".form__input",
     buttonSelector: ".form__button",
+    errorMsgSelector: ".form__error-msg",
     inputErrorClass: "form__input_error",
     errorMsgPrefix: "error",
     errorMsgVisibleClass: "form__error-msg_visible",
